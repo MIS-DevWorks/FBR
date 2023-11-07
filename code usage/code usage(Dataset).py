a22ad85c3f3a4f696fab6d05175fb647a2b6293f
Ranger <28769811+tiongleslie@users.noreply.github.com>
2023-11-07 19:49:08 +0900

    Update code usage(Dataset).py

--- code usage/code usage(Dataset).py
+++ code usage/code usage(Dataset).py
@@ -3,7 +3,7 @@ from utils.dataset import VGGFace2_dataset
 
 
 dataset = VGGFace2_dataset(csv_path="../data/MAAD_Face_filtered_train.csv",
-                           img_path='/media/leslie/samsung/Biometrics/VGGFace2/crop/train/',
+                           img_path='../data/VGGFace2/train/',
                            train_augmentation=True, image_size=112)
 data_loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=4)
 
